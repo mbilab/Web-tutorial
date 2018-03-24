fad891b617379736283293aaa3a8cffa6cf93389
poyushen <accompany0723@gmail.com>
2018-03-24 16:56:43 +0800

    simplify app.js

--- unit/js/demo2/app.js
+++ unit/js/demo2/app.js
@@ -5,7 +5,7 @@ const getZIndex = e => {
 }
 var x1, x2, y1, y2 = 0;
 var info = {'leftpic':'#left', 'starpic':'#star', 'hugpic':'#hug', 'subwaypic':'#subway'}
-var pic = ['leftpic','starpic','hugpic','subwaypic']
+var pic = ['leftpic', 'starpic', 'hugpic', 'subwaypic']
 
 $('#items > img')
   .bind('dragstart', e => {
@@ -20,15 +20,15 @@ $('#items > img')
     y2 = e.originalEvent.pageY
 
     if ((x2 > x1) && (Math.abs(y1 - y2) < 50)) { //! check the logic
-      for(var i=0; i<4; i++){
+      for(var i = 0; i < 4; i ++){
         if(e.target.id == pic[i]){
-          $(e.target).animate({marginLeft:(3*i+3)+'%',width:(38-2*i)+'vw',marginTop:i+'%',zIndex:(4-i)}, 300)
-            .prev().animate({marginLeft:'3%', width:'38vw', zIndex:'4'}, 500);
+          $(e.target).animate({marginLeft: (3 * i + 3) + '%', width: (38 - 2 * i) + 'vw', marginTop: i + '%', zIndex: (4 - i)}, 300)
+            .prev().animate({marginLeft: '3%', width: '38vw', zIndex: '4'}, 500);
         }
       }
 
-      $(info[e.target.id]).animate({marginLeft:'100%', opacity:'0'},300)
-        .prev().animate({marginLeft:'0%'}, 500);
+      $(info[e.target.id]).animate({marginLeft: '100%', opacity: '0'}, 300)
+        .prev().animate({marginLeft: '0%'}, 500);
     }
   })
   .click(e => {
@@ -36,14 +36,14 @@ $('#items > img')
     $(e.target).animate({marginLeft: '-100%'}, 600) //! check the animation
       .next().animate({marginLeft: '3%', marginTop:'0%', opacity: 1, width: '38vw', zIndex: 4}, 800);
 
-    $(info[e.target.id]).animate({marginLeft:'-100%'}, 600)
-      .next().animate({marginLeft:'0%', opacity:'1'}, 600);
+    $(info[e.target.id]).animate({marginLeft: '-100%'}, 600)
+      .next().animate({marginLeft: '0%', opacity: '1'}, 600);
   })
 
 $('.shopcar').bind('drop', e => {
   $('#items > img').remove()
   $('.info').remove()
-  $('#add').animate({marginTop:'-60%', opacity:'1'}, 600)
+  $('#add').animate({marginTop: '-60%', opacity: '1'}, 600)
   $('.shopcar').animate({width: '8vw', height: '8vw'}, 200)
   $('.shopcar').animate({width: '6vw', height: '6vw'}, 200)
 })
