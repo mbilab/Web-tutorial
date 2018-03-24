71cca85c4f75319210b27d56cc590f7d4d02b7e6
poyushen <accompany0723@gmail.com>
2018-03-24 16:46:14 +0800

    simplify app.js

--- unit/js/demo2/app.js
+++ unit/js/demo2/app.js
@@ -3,11 +3,9 @@ const getZIndex = e => {
   if (isNaN(z)) return window.getZIndex(e.parentNode)
   return z
 }
-
-var x1 = 0
-var x2 = 0
-var y1 = 0
-var y2 = 0
+var x1, x2, y1, y2 = 0;
+var info = {'leftpic':'#left', 'starpic':'#star', 'hugpic':'#hug', 'subwaypic':'#subway'}
+var pic = ['leftpic','starpic','hugpic','subwaypic']
 
 $('#items > img')
   .bind('dragstart', e => {
@@ -16,32 +14,21 @@ $('#items > img')
   })
 
   .bind('dragend', e => {
+    if(e.target.id == 'leftpic') return
+
     x2 = e.originalEvent.pageX
     y2 = e.originalEvent.pageY
+
     if ((x2 > x1) && (Math.abs(y1 - y2) < 50)) { //! check the logic
-
-      var topinfo;
-
-      switch(e.target.id){
-        case 'starpic':
-          $(e.target).animate({marginLeft:'6%', width:'36vw', marginTop:'1%', zIndex:'3'},300);
-          topinfo=$('#star');
-          break;
-
-        case 'hugpic':
-          $(e.target) .animate({marginLeft:'9%', width:'34vw', marginTop:'2%', zIndex:'2'}, 300);
-          topinfo=$('#hug');
-          break;
-
-        case 'subwaypic':
-          $(e.target) .animate({marginLeft:'12%', width:'32vw', marginTop:'3%', zIndex:'1'}, 300);
-          topinfo=$('#subway');
-          break;
+      for(var i=0; i<4; i++){
+        if(e.target.id == pic[i]){
+          $(e.target).animate({marginLeft:(3*i+3)+'%',width:(38-2*i)+'vw',marginTop:i+'%',zIndex:(4-i)}, 300)
+            .prev().animate({marginLeft:'3%', width:'38vw', zIndex:'4'}, 500);
+        }
       }
 
-      $(e.target).prev().animate({marginLeft:'3%', width:'38vw', zIndex:'4'}, 500);
-      topinfo.animate({marginLeft:'100%', opacity:'0'},300);
-      topinfo.prev().animate({marginLeft:'0%'}, 500);
+      $(info[e.target.id]).animate({marginLeft:'100%', opacity:'0'},300)
+        .prev().animate({marginLeft:'0%'}, 500);
     }
   })
   .click(e => {
@@ -49,27 +36,8 @@ $('#items > img')
     $(e.target).animate({marginLeft: '-100%'}, 600) //! check the animation
       .next().animate({marginLeft: '3%', marginTop:'0%', opacity: 1, width: '38vw', zIndex: 4}, 800);
 
-    var topinfo;
-
-    switch(e.target.id){
-      case 'leftpic':
-        topinfo = $('#left');
-        break;
-
-      case 'starpic':
-        topinfo = $('#star');
-        break;
-
-      case 'hugpic':
-        topinfo = $('#hug');
-        break;
-
-      case 'subwaypic':
-        topinfo = $('#subway');
-        break;
-    }
-    topinfo.animate({marginLeft:'-100%'}, 600);
-    topinfo.next().animate({marginLeft:'0%', opacity:'1'}, 600);
+    $(info[e.target.id]).animate({marginLeft:'-100%'}, 600)
+      .next().animate({marginLeft:'0%', opacity:'1'}, 600);
   })
 
 $('.shopcar').bind('drop', e => {
