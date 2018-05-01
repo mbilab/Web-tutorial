d64be55b6bac0a07d4812253c0171b8e3a9368ba
dada8397 <vincent8397@gmail.com>
2018-05-01 23:01:06 +0800

    Modify db example

--- unit/db/index.js
+++ unit/db/index.js
@@ -1,47 +1,33 @@
 const insert_data = $form => {
-  id: $form.find('.id').val(),
-  name: $form.find('.name').val(),
-  age: $form.find('.age').val(),
+  return {
+    id: $form.find('input[name="id"]').val(),
+    name: $form.find('input[name="name"]').val(),
+    age: $form.find('input[name="age"]').val(),
+  }
 }
 
-$('#mysql_insert_submit').click(() => {
-  //! why php_? where the .php file?
-  $.post('php_mysql_insert.php', insert_data($('#mysql_insert')), data => {
+$('#mysql_insert .submit').click(() => {
+  $.post('php/mysql_insert.php', insert_data($('#mysql_insert')), data => {
     $('#mysql_insert .result').html(data)
   })
 })
 
-$('#mysql_query_submit').click(() => {
-  $.post('php_mysql_query.php', {name: $('#mysql_query .name').val()}, data => {
-      $('#mysql_query_result').html(data)
+$('#mysql_query .submit').click(() => {
+  $.post('php/mysql_query.php', {name: $('#mysql_query input[name="name"]').val()}, data => {
+    $('#mysql_query_result').html(data)
   })
 })
 
-$('#mongo_insert_submit').click(() => {
-  $.post({
-    url: "php_mongo_insert.php",
-    type: "post",
-    data: {
-      name: $('#mongo_insert_name').val(),
-      age: $('#mongo_insert_age').val()
-    },
-    success: (res) => {
-      $('#mongo_insert_result').html(res);
-    }
-  });
-});
-
-$("#mongo_query_submit").click(() => {
-  $.post({
-    url: "php_mongo_query.php",
-    type: "post",
-    data: {
-      name: $('#mongo_query_name').val(),
-    },
-    success: (res) => {
-      $('#mongo_query_result').html(res);
-    }
+$('#mongo_insert .submit').click(() => {
+  $.post('php/mongo_insert.php', insert_data($('#mongo_insert')), data => {
+    $('#mongo_insert .result').html(data)
   })
-});
+})
+
+$("#mongo_query .submit").click(() => {
+  $.post('php/mongo_query.php', {name: $('#mongo_query input[name="name"]').val()}, data => {
+    $('#mongo_query .result').html(data)
+  })
+})
 
 // vi:et:sw=2:ts=2
