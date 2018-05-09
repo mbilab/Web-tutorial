8a1b91d29891e086d7f655804f584d9f5bb8fd87
dada8397 <vincent8397@gmail.com>
2018-05-09 16:20:57 +0800

    Use fetch_all

--- unit/db/mysql.php
+++ unit/db/mysql.php
@@ -3,29 +3,27 @@ $config = include('config.php');
 
 $mysqli = new mysqli($config['mysql']['host'], $config['mysql']['username'], $config['mysql']['passwd'], $config['mysql']['dbname']);
 if ($mysqli->connect_errno) {
-  die 'fail to connect: ' . $mysqli->connect_error;
+  die('fail to connect: ' . $mysqli->connect_error);
 }
 
 if ($_REQUEST['id']) { # insert
   $sql = "INSERT INTO `student_course` (`sid`, `cid`) VALUES ('{$_POST['id']}', '{$_POST['course']}');";
   if (!$mysqli->query($sql)) {
-    die 'fail to insert: ' . $mysqli->error;
+    die('fail to insert: ' . $mysqli->error);
   }
 } else { # query
   $sql = "SELECT student.id, student.name FROM student
     JOIN student_course ON student_course.sid = student.id
     JOIN course ON course.id = student_course.cid
     WHERE course.name LIKE '{$_REQUEST['name']}'";
-  #! use mysqli_fetch_all()
   if ($result = $mysqli->query($sql)) {
-    $students = "";
-    while ($student = $result->fetch_row()) {
-      $students = $students . $student[1] . ", ";
-    }
-    $students = substr($students, 0, -2);
+    $students = $result->fetch_all();
+    $students = implode(", ", array_map(function ($entry) {
+          return $entry[1];
+    }, $students));
     echo $students;
   } else {
-    die 'fail to query: ' . $mysqli->error;
+    die('fail to query: ' . $mysqli->error);
   }
 }
 
