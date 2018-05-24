e825f59802a264b0f63098a5e5837c742916e6f9
dirty <darby.chang@gmail.com>
2018-05-24 21:18:23 +0800

    add npc

--- unit/game/app.js
+++ unit/game/app.js
@@ -1,43 +1,46 @@
 const game = new Phaser.Game(320, 240, Phaser.AUTO, 'game', {
 
   preload: () => {
-    game.load.spritesheet('player', './res/player.png', 41, 36, 12)
+    game.load.spritesheet('player', './res/healer_f.png', 32, 36, 12)
+    game.load.spritesheet('npc', './res/healer_m.png', 32, 36, 12)
   },
 
   create: () => {
     game.stage.backgroundColor = '#4488AA'
 	  keyboard = game.input.keyboard.createCursorKeys()
 
-    sprite = game.add.sprite(1, 1, 'player', 0)
-    game.physics.enable(sprite, Phaser.Physics.ARCADE)
+    npc = game.add.sprite(64, 72, 'npc', 7)
+    player = game.add.sprite(0, 0, 'player', 7)
+    game.physics.enable(player, Phaser.Physics.ARCADE)
 
-    sprite.body.collideWorldBounds = true
+    player.body.collideWorldBounds = true
 
-    sprite.animations.add('moveDown',  [0,  1,  2], 10, true)
-    sprite.animations.add('moveLeft',  [9, 10, 11], 10, true)
-    sprite.animations.add('moveRight', [3,  4,  5], 10, true)
-    sprite.animations.add('moveUp',    [6,  7,  8], 10, true)
+    player.animations.add('moveUp',    [0,  1,  2], 10, true)
+    player.animations.add('moveRight', [3,  4,  5], 10, true)
+    player.animations.add('moveDown',  [6,  7,  8], 10, true)
+    player.animations.add('moveLeft',  [9, 10, 11], 10, true)
   },
 
   update: () => {
     speed = 2
     if (keyboard.up.isDown) {
-      sprite.y -= speed
-      sprite.play('moveUp')
+      player.y -= speed
+      player.play('moveUp')
     } else if (keyboard.down.isDown) {
-      sprite.y += speed
-      sprite.play('moveDown')
+      player.y += speed
+      player.play('moveDown')
     } else if (keyboard.left.isDown) {
-      sprite.x -= speed
-      sprite.play('moveLeft')
+      player.x -= speed
+      player.play('moveLeft')
     } else if (keyboard.right.isDown) {
-      sprite.x += speed
-      sprite.play('moveRight')
+      player.x += speed
+      player.play('moveRight')
     } else
-      sprite.animations.stop()
+      player.animations.stop()
   },
 
   render: () => {
+    // game.debug.body(player)
   },
 
 })
