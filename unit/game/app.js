28e333c9d4c7ca9f99b25fe46ba4f5e21e7fb74a
goho302jo03 <goho302jo03@gmail.com>
2018-05-24 11:20:31 +0800

    remove assets

--- unit/game/app.js
+++ unit/game/app.js
@@ -1,7 +1,7 @@
 const game = new Phaser.Game(320, 240, Phaser.AUTO, 'game', {
 
   preload: () => {
-    game.load.spritesheet('player', './assets/img/player.png', 32, 32)
+    game.load.spritesheet('player', './res/player.png', 41, 36, 12)
   },
 
   create: () => {
@@ -14,9 +14,9 @@ const game = new Phaser.Game(320, 240, Phaser.AUTO, 'game', {
     sprite.body.collideWorldBounds = true
 
     sprite.animations.add('moveDown',  [0,  1,  2], 10, true)
-    sprite.animations.add('moveLeft',  [3,  4,  5], 10, true)
-    sprite.animations.add('moveRight', [6,  7,  8], 10, true)
-    sprite.animations.add('moveUp',    [9, 10, 11], 10, true)
+    sprite.animations.add('moveLeft',  [9, 10, 11], 10, true)
+    sprite.animations.add('moveRight', [3,  4,  5], 10, true)
+    sprite.animations.add('moveUp',    [6,  7,  8], 10, true)
   },
 
   update: () => {
