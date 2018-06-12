13fd759574c214c5ecbc0cea39374bf9b585e278
poyushen <accompany0723@gmail.com>
2018-06-12 22:05:26 +0800

    add functions in rtc.js

--- unit/sound/rtc.js
+++ unit/sound/rtc.js
@@ -1,19 +1,65 @@
-'use strict';
-
-var constraints = {
-  video: true
+function getUserMedia(options, successCallback, failureCallback) {
+    var api = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    if (api) {
+      return api.bind(navigator)(options, successCallback, failureCallback);
+    }
+    alert('User Media API not supported.');
 }
 
-var video = document.querySelector('video');
+var theStream;
+var theRecorder;
+var recordedChunks = [];
 
-function handleSuccess(stream){
-  window.stream = stream;
-  video.srcObject = stream;
+
+function getStream(){
+  var constraints = {video: true, audio: true};
+  getUserMedia(constraints, function (stream) {
+    var mediaControl = document.querySelector('video');
+    if (navigator.mozGetUserMedia) {
+      mediaControl.mozSrcObject = stream;
+    }
+    else{
+      mediaControl.srcObject = stream;
+      mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
+    }
+
+    theStream = stream;
+    try {
+      recorder = new MediaRecorder(stream, {mimeType: 'video/webm'});
+    } catch (e) {
+      console.error('Exception while creating MediaRecorder: ' + e);
+      return;
+    }
+    theRecorder = recorder;
+    console.log('MediaRecorder created');
+    recorder.ondataavailable = recorderOnDataAvailable;
+    recorder.start(100);
+  }, function(err){
+    alert('Error: ', err);
+  });
 }
 
-function handleError(error){
-  console.log('getUserMedia error: ', error);
+function recorderOnDataAvailable (event){
+  if (event.data.size == 0) return;
+  recordedChunks.push(event.data);
 }
 
-navigator.mediaDevices.getUserMedia(constraints).
-    then(handleSuccess).catch(handleError);
+function stop(){
+  theStream.getTracks().forEach((track) => {track.stop();})
+}
+
+function download() {
+  console.log('Saving data');
+  theRecorder.stop();
+  theStream.getTracks()[0].stop();
+
+  var blob = new Blob(recordedChunks, {type: "video/webm"});
+  var url = (window.URL || window.webkitURL).createObjectURL(blob);
+  var a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+  a.href = url;
+  a.download = 'test.webm';
+  a.click();
+}
