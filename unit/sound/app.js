14e7666e5f4ee2000813ef3cca6c664cbd187a5c
dirty <darby.chang@gmail.com>
2018-06-12 23:10:13 +0800

    sound done

--- unit/sound/app.js
+++ unit/sound/app.js
@@ -1,10 +1,36 @@
+const type = 'audio' // try 'vedio'
+const ext = 'video' === type ? 'mp4' : 'webm'
+const recordedChunks = []
 navigator.mediaDevices.getUserMedia({
   audio: true,
-  video: true,
+  video: 'vedio' === type,
 }).then(stream => {
-  // window.stream = stream
-  document.querySelector('#stream-video').srcObject = stream
+  if ('vedio' === type)
+    document.querySelector('#stream-video').srcObject = stream
+  window.recorder = new MediaRecorder(stream, {mimeType: `${type}/${ext}`})
+  recorder.ondataavailable = event => {
+    if (0 === event.data.size) return
+    recordedChunks.push(event.data)
+  }
 }).catch(err => console.log(err))
+const start = () => {
+  recordedChunks.length = 0
+  recorder.start(100) // a chunk per 100 ms
+}
+const stop = () => {
+  if ('inactive' === recorder.state) return
+  recorder.stop()
+}
+const download = () => {
+  stop()
+  let blob = new Blob(recordedChunks, {type: `${type}/${ext}`})
+  let a = document.createElement('a')
+  document.body.appendChild(a)
+  a.download = `test.${ext}`
+  a.href = window.URL.createObjectURL(blob)
+  a.click()
+  document.body.removeChild(a)
+}
 
 const speak = () => {
   const utter = new SpeechSynthesisUtterance()
