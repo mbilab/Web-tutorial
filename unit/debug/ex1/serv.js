790cc0eb5ca2646c1d06af79929131adebe9bd8c
cheese <goho302jo03@gmail.com>
2018-09-29 14:39:51 +0800

    remove useless code in debug/ex1/serv.js

--- unit/debug/ex1/serv.js
+++ unit/debug/ex1/serv.js
@@ -1,15 +1,10 @@
 const express = require("express")
 const app = express()
 const port = 50000 // 請改成其他你喜歡的數字
-const bodyParser = require("body-parser");
 
 var fs = require("fs");
 var content = fs.readFileSync("./students.json");
 var jsonfile = JSON.parse(content);
-app.use(bodyParser.json());       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
 
 app.listen(port)
 app.use(express.static(__dirname + "/public"))
