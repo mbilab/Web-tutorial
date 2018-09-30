1224e36ca51aa4c820edb3dd6b99898594ade5c2
cheese <goho302jo03@gmail.com>
2018-09-30 22:21:37 +0800

    add unit debug/ex4
    This is a html/css/js debug example. There are three points to be
    corrected in this web.
    1. Uncaught ReferenceError: $ is not define
    2. When clicking the button, the red box should move to right and
       back to origin.
    3. If we continously click the button, the animation will have a
       delay.

---
+++ unit/debug/ex4/app.js
@@ -0,0 +1,12 @@
+$("#animate").click(function(){
+  $("#move_box").animate({
+    width: "300",
+    height: "300",
+    left: "200",
+  }, 1500 );
+  $("#move_box").animate({
+    width: "100",
+    height: "100",
+    left: "0",
+  }, 1500 );
+})
