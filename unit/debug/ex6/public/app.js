1f009cc2d02972e5d734cc63a833e81caf13add2
dirty <darby.chang@gmail.com>
2018-11-01 09:00:29 +0800

    debug/ex6/ code

---
+++ unit/debug/ex6/public/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('[type=submit]').click(e => {
+    e.preventDefault()
+    $.getJSON('./ajax', {
+      id1: $('[name=id1]').val(),
+      id2: $('[name=id2]').val(),
+    }, res => {
+      $('[name=name1]').val(res.name1)
+      $('[name=name2]').val(res.name2)
+    })
+  })
+})
+
+// vi:et:sw=2:ts=2
--- unit/debug/ex6/serv.js
+++ unit/debug/ex6/serv.js
@@ -1,17 +1,16 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 const port = 50000 // modify this if necessary
-
-var fs = require("fs");
-var content = fs.readFileSync("./students.json");
-var jsonfile = JSON.parse(content);
 
 app.listen(port)
 console.log(`listening port: ${port}`)
 
-app.use(express.static(__dirname + "/public"))
+const fs = require('fs')
+const students = JSON.parse(fs.readFileSync('./students.json'))
 
-app.get("/ajax_list", function(req, res) {
-  content = JSON.stringify(jsonfile['class1']) + JSON.stringify(jsonfile['class2'])
-  res.send(`${content}`)
+app.use(express.static(__dirname + '/public'))
+
+app.get('/ajax', function(req, res) {
+  const json = JSON.stringify(students[req.query.id1]) + JSON.stringify(students[req.query.id2])
+  res.send(json)
 })
