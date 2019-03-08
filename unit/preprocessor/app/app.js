efb527119f80e80e8c113a9fef1beebb0609cd80
yichung <ql3895619@gmail.com>
2019-03-08 15:39:34 +0800

    add more pug sass and es6 standard

--- unit/preprocessor/app/app.js
+++ unit/preprocessor/app/app.js
@@ -1,9 +1,16 @@
 import './app.sass'
 import '@babel/polyfill'
 
+const jquery = require("jquery");
+window.$ = window.jQuery = jquery;
+
 // use something new in js
-// here we choose `async` at Nov 2017
-const f = async () => {
-    document.getElementById('title').style.color = 'red'
-}
-f()
+// here we choose`Spread operator` and `Template literals`
+
+const stuendts = ['Jine', 'Tom', 'Anna']
+const teachers = ['Tony', 'Aaron']
+const people = [...teachers, ... stuendts]
+
+$(document).ready(function() {
+  $('#button').click(()=>console.log(`There are ${people} in the classroom`))
+})
