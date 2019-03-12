7a7ecd387a5d668ed72dd879c50fb458cb42f59b
yichung <ql3895619@gmail.com>
2019-03-12 13:42:04 +0800

    refine preprocessor/app/app.js

--- unit/preprocessor/app/app.js
+++ unit/preprocessor/app/app.js
@@ -1,20 +1,8 @@
-import './app.sass'
-// step 5 : learn new Javascript standard
-// Use new feature in es6, Module, importing './app.sass'.
-
-// step 4 : use jQuery with Parcel
+// Using parcel with jquery need some tricks.
 // Read : https://stackoverflow.com/questions/47968529/how-do-i-use-jquery-and-jquery-ui-with-parcel-bundler
 const jquery = require("jquery")
 window.$ = window.jQuery = jquery
 
-// step 5 : learn new Javascript standard
-// Read the following example and google es6.
-// Here are `Spread operator` and `Template literals`
-
-const stuendts = ['Jine', 'Tom', 'Anna']
-const teachers = ['Tony', 'Aaron']
-const people = [...teachers, ...stuendts]
-
 $(document).ready(function() {
-  $('button').click(()=>console.log(`There are ${people} in the classroom`))
+  $('button').click(()=>console.log('Good job'))
 })
