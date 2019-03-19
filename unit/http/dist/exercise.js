ff635b97584902b0c44c3c1927648cf29a29b377
yichung <ql3895619@gmail.com>
2019-03-19 09:30:59 +0800

    clear ser.js and dist/

--- unit/http/dist/exercise.js
+++ unit/http/dist/exercise.js
@@ -1,26 +1,7 @@
 $(document).ready(function() {
   $('#ajax-form button[type="submit"]').click((event) => {
-    /* Step 8:
-     * try comment this line
-     *  the default non-ajax request will be performed
-     *  the whole page is returned, which is not what we want
-     * not necessary if button type is not `submit`
-     *  try remove `type="submit"` in the last code snippet
-     * but make the form work without ajax is a good practice
-     */
     event.preventDefault()
-
-    $.get('./step5')
-    /*
-    $.get('./step5',{
-        fname: $("input[name='fName']").val(),
-        lname: $("input[name='lName']").val()
-    }, (data) => {
-        $('#ajax-output').html(data)
-    })
-    */
-
-
+    // Step 9 and step 10 code goes here
 
     // Step 11 code goes here
   })
