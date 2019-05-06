98c46e13420998b94a0bf41ca64240fbd25ff89d
yiz <ql3895619@gmail.com>
2019-05-06 21:51:11 +0800

    remove redandunt code

--- unit/visualization/app/d3.js
+++ unit/visualization/app/d3.js
@@ -4,26 +4,15 @@ const data = {
 }
 
 $(() => {
-  /* Step 1:
-   * 使用 css selector 加上 .select() 選取要加入資料的元件
-   */
+  // Step 1:
 
   $('button').click(e => {
     const code = $(e.target).data('code')
-    /* Step 2:
-     * 連結資料
-     */
+    // Step 2:
 
-    /* Step 3:
-     * .exit() 會過濾出沒有資料配對的物件
-     * .remove() 會把這些物件刪掉
-     */
+    // Step 3:
 
-    /* Step 4:
-     * .enter() 為沒物件的資料建立物件
-     * .append() 跟 jQuery 的 append 類似，加入 DOM 元件。
-     * d3 元件裡的資料跟索引會傳進 `(v, i) => {}` 裡的 v 跟 i
-     */
+    // Step 4:
   })
   $('button:first-child').click()
 
--- unit/visualization/app/vue.vue
+++ unit/visualization/app/vue.vue
@@ -1,19 +1,15 @@
 <template lang="pug">
 .v-app
   // Step 2.2
-  // 你的輸入會跟 inputData 雙向綁定，並在螢幕顯示
 
   // Step 3.1
-  // 使用 v-for，網頁內容隨著資料迭代渲染。
 
-  // Step 3.2
-  // 完成 v-for 的語法，定義裡的value跟idx
   svg#chart(height="560",width="960")
-    svg(v-for="[value and idx of data]",:x="60 + idx * 150")
-      text(x="20",:y="440 - 400 * value") {{(value * 100).toFixed(1)}}%
-      rect(:height="value * 400",width="100",:y='450 - value * 400')
+    // Step 3.2
+    // svg(v-for="[value and idx of data]",:x="60 + idx * 150")
+    //   text(x="20",:y="440 - 400 * value") {{(value * 100).toFixed(1)}}%
+    //   rect(:height="value * 400",width="100",:y='450 - value * 400')
   // step 4.1
-  // 加入 button
 </template>
 
 <script>
@@ -26,7 +22,6 @@ export default {
   data() { return {
     chart: this.N2,
     // Step 2.1
-    // 加入變數
 
     N2: [.2, .2, .2, .2, .2],
     P7: [.2, .2, .2, .2, .1, .1],
@@ -36,7 +31,6 @@ export default {
     update(code) {
       this.inputData = `你按了${code}的按鈕`
     // step 4.2
-    // 換你寫了
     },
   },
 
