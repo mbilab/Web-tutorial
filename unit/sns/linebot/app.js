194f236d0d05d0c42e5cc05f43492b4865eececf
low0621 <low0621@gmail.com>
2020-05-06 21:33:54 +0800

    Feat: Add linebot demo

---
+++ unit/sns/linebot/app.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const https = require('https')
+const express = require('express')
+const app = express()
+const line = require('@line/bot-sdk')
+const config = require('./config.js')
+
+const lineConfig = {
+  channelAccessToken: config.accessToken,
+  channelSecret: config.secret
+}
+
+const sslOptions = {
+  key: fs.readFileSync(config.key_path),
+  ca: fs.readFileSync(config.ca_path),
+  cert: fs.readFileSync(config.cert_path)
+}
+
+app.post('/webhook', line.middleware(lineConfig), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+})
+
+const client = new line.Client(lineConfig)
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null)
+  }
+
+  if (event.message.id == '100001') return 200 // to deal with verify dummy data
+
+  return client.replyMessage(event.replyToken, { 
+    type: 'text',
+    text: event.message.text
+  })
+}
+
+
+const server = https.createServer(sslOptions, app)
+
+server.listen(config.port, () => {
+	console.log(`listen on port ${config.port}`)
+})
---
+++ unit/sns/linebot/config.sample.js
@@ -0,0 +1,10 @@
+config = {
+  'accessToken': 'your channel access token',
+  'ca_path': 'path of ca',
+  'cert_path': 'path of cert',
+  'key_path': 'path of private key',
+  'port': 'your bot\'s port',
+  'secret': 'your channel secret'
+}
+
+module.exports = config;
